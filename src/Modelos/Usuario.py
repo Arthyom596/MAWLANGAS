6762c3f9cfdf4007cf0bf2d2fe7b9fadfd116947
Arthyom596 <luiseduardo596596@gmail.com>
2025-04-22 15:20:34 -0600

    Nuevo Modelo

--- src/Modelos/Usuario.py
+++ src/Modelos/Usuario.py
@@ -5,14 +5,14 @@ nombre_bd = "Mawlangas.db"
 def conectar():
     return sqlite3.connect(nombre_bd)
 
-def crear_usuario(usuario, contrasena, nombre, apellido_p, apellido_m):
+def crear_usuario(usuario, contrasena, nombre, apellido_p, apellido_m,correo):
     conexion = conectar()
     cursor = conexion.cursor()
     try:
         cursor.execute("""
             INSERT INTO Usuarios (Usuario, Contrasena, Nombre, ApellidoPaterno, ApellidoMaterno)
             VALUES (?, ?, ?, ?, ?)
-        """, (usuario, contrasena, nombre, apellido_p, apellido_m))
+        """, (usuario, contrasena, nombre, apellido_p, apellido_m,correo))
         conexion.commit()
         print("Usuario creado correctamente.")
     except sqlite3.IntegrityError:
@@ -27,14 +27,6 @@ def obtener_usuarios():
     usuarios = cursor.fetchall()
     conexion.close()
     return usuarios
-
-def buscar_usuario_por_nombre(nombre_usuario):
-    conexion = conectar()
-    cursor = conexion.cursor()
-    cursor.execute("SELECT * FROM Usuarios WHERE Usuario = ?", (nombre_usuario,))
-    usuario = cursor.fetchone()
-    conexion.close()
-    return usuario
 
 def actualizar_usuario(id_usuario, usuario, contrasena, nombre, apellido_p, apellido_m,correo):
     conexion = conectar()
