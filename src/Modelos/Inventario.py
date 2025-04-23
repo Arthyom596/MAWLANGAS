cbfb06e6f73ef3c5a7093d682e9acf9354580c9d
Arthyom596 <luiseduardo596596@gmail.com>
2025-04-22 22:28:53 -0600

    Nuevo Modelo

---
+++ src/Modelos/Inventario.py
@@ -0,0 +1,53 @@
+import sqlite3
+
+nombre_bd = "Mawlangas.db"
+
+def conectar():
+    conexion = sqlite3.connect(nombre_bd)
+    cursor = conexion.cursor()
+    return conexion
+
+# Crear inventario
+def crear_inventario(id_producto, id_sabor, cantidad, fecha_actualizacion):
+    conexion = conectar()
+    cursor = conexion.cursor()
+    try:
+        cursor.execute("""
+        INSERT INTO Inventario (IDProducto, IDSabor, Cantidad, FechaActualizacion)
+        VALUES (?, ?, ?, ?)
+        """, (id_producto, id_sabor, cantidad, fecha_actualizacion))
+        conexion.commit()
+        print("Inventario creado correctamente.")
+    except sqlite3.IntegrityError:
+        print("Error: No se pudo crear el inventario (posible clave duplicada o conflicto).")
+    finally:
+        conexion.close()
+
+# Obtener inventarios
+def obtener_inventarios():
+    conexion = conectar()
+    cursor = conexion.cursor()
+    cursor.execute("SELECT * FROM Inventario")
+    inventarios = cursor.fetchall()
+    conexion.close()
+    return inventarios
+
+# Actualizar inventario
+def actualizar_inventario(id_inventario, id_producto, id_sabor, cantidad, fecha_actualizacion):
+    conexion = conectar()
+    cursor = conexion.cursor()
+    cursor.execute("""
+    UPDATE Inventario
+    SET IDProducto = ?, IDSabor = ?, Cantidad = ?, FechaActualizacion = ?
+    WHERE IDInventario = ?
+    """, (id_producto, id_sabor, cantidad, fecha_actualizacion, id_inventario))
+    conexion.commit()
+    conexion.close()
+
+# Eliminar inventario
+def eliminar_inventario(id_inventario):
+    conexion = conectar()
+    cursor = conexion.cursor()
+    cursor.execute("DELETE FROM Inventario WHERE IDInventario = ?", (id_inventario,))
+    conexion.commit()
+    conexion.close()
